3f00caeadeb20b0e319f344bec42c048b9740c9d
thinksky5124 <thinksky@mail.dlut.edu.cn>
2021-05-26 10:49:23 +0800

    first commit

---
+++ armordetetor.cpp
@@ -0,0 +1,6 @@
+#include "armordetetor.h"
+
+ArmorDetetor::ArmorDetetor()
+{
+
+}
---
+++ armordetetor.h
@@ -0,0 +1,11 @@
+#ifndef ARMORDETETOR_H
+#define ARMORDETETOR_H
+
+
+class ArmorDetetor
+{
+public:
+    ArmorDetetor();
+};
+
+#endif // ARMORDETETOR_H

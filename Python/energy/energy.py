daddfc4410bcc90549e4ee66d239fc7ed6818534
Tongbh <1308751722@qq.com>
2021-06-05 23:55:39 +0800

    更新SVM识别功能

--- Python/energy/energy.py
+++ Python/energy/energy.py
@@ -2,7 +2,7 @@
 Author: Thyssen Wen
 Date: 2021-06-04 20:34:30
 LastEditors: Tongbh111
-LastEditTime: 2021-06-04 23:41:03
+LastEditTime: 2021-06-05 23:51:42
 Description: energy hit module
 FilePath: \DLLG-2021-BUG-CV\Python\energy\energy.py
 '''
@@ -17,7 +17,8 @@ class color():
     """
     BLUE = 0
     RED = 1
-    
+
+
 class Rectangle:
     def __init__(self,x,y,w,h):
         self.width=w
@@ -38,6 +39,7 @@ class Energy():
         self.enermy_color = enermy_color
         self.energy_armor_area_min = int(config.getConfig("energy", "energy_armor_area_min_threshold"))
         self.energy_armor_area_max = int(config.getConfig("energy", "energy_armor_area_max_threshold"))
+        self.svm_model_path = config.getConfig("energy", "svm_model_path")
         if self.enermy_color == color.BLUE:
             blue_hmin = int(config.getConfig("energy", "hsv_blue_hmin"))
             blue_hmax = int(config.getConfig("energy", "hsv_blue_hmax"))
@@ -67,15 +69,30 @@ class Energy():
             self.Upper = np.array([180, 30, 255])#要识别的颜色的上限
         
     def foundHitleave(self,img):
-        # process
+        # TODO: add predict functions to match the speed of rotation(Kalman Filter)
+        # main process of energy detection
         processImage = self.preImageProcess_hsv(img)
         logging.info("pre Process Image using hsv color space success!")
-        hitLeaves = self.findAndfilterContours(processImage)
-        logging.info("find "+str(len(hitLeaves))+" lightBars in current frame")
-        
-        return hitLeaves
+        target_x, target_y = self.findAndfilterContours(processImage)
+        logging.info("target is x: " + str(target_x) + " y: " + str(target_y))
+
 
     def findAndfilterContours(self,img):
+        """
+        Function for detect the windmills in the image
+
+        Find all the contours in the image
+        Transform the ROI with a proper area into regular rectangle
+        Judge the type of the ROI with SVM
+        Find sub contours of the ROI, get the center point we need to strike
+        The output is a tuple(int, int), the center point of armor
+
+        Args:
+            img (cv::mat): src image
+
+        Returns:
+            target_x, target_y (tuple(int, int)): center point of armor
+        """
         imgdraw = img.copy()
         imgdraw = cv2.cvtColor(imgdraw, cv2.COLOR_GRAY2BGR)
         # initial
@@ -123,7 +140,15 @@ class Energy():
                     perspective_mat = cv2.getPerspectiveTransform(rect_src, rect_dst)
                     warped_img = cv2.warpPerspective(img, perspective_mat, (int(width), int(height)))
                     cv2.imshow("roi", warped_img)
+                    # 将所得能量机关扇叶图像送入SVM识别器中
+                    type = self.judgeEnergyType(warped_img)
+                    if type == 1 and hierarchy[0][i][2] > 0:
+                        strike_area = cv2.minAreaRect(contours[hierarchy[0][i][2]])
+                        target_x, target_y = strike_area[0]
+                        cv2.circle(imgdraw, (int(target_x), int(target_y)), 10, (0,0,255), -1)
+                    cv2.imshow("imgdraw", imgdraw)
 
+        return target_x, target_y
         # for i in contours:#遍历所有的轮廓
         #     x,y,w,h = cv2.boundingRect(i)#将轮廓分解为识别对象的左上角坐标和宽、高
         #     if w*h > self.energy_armor_area_min and w*h < self.energy_armor_area_max:
@@ -137,12 +162,13 @@ class Energy():
 
         Transform the input image into hsv model.
         Use filters to remove the irrelative color pixels
+        The output is a binary mat with one color
 
         Args:
             img (cv::mat): src image
 
         Returns:
-            image (cv::mat): pre process image
+            image_result (cv::mat): pre process image
         """
         # 初始化
         kernel1 = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
@@ -158,7 +184,6 @@ class Energy():
         img_dilate = cv2.dilate(thr, kernel1)
         if self.enermy_color == color.BLUE:
             mask = cv2.inRange(img_hsv, self.Lower, self.Upper)
-
         else:
             # 红色需要根据两个阈值进行判断，使用或运算将两个区域相加
             mask1 = cv2.inRange(img_hsv, self.Lower, self.Upper)
@@ -172,12 +197,36 @@ class Energy():
         
         return img_result
 
+    def judgeEnergyType(self, img):
+        """
+        Judge the type of the energy windmill
 
+        Use Hog descriptor to get the feature of image input
+        Load trained SVM model to predict type
+        Output the predict type(1 for windmill need to strike, 2 for already striken)
 
+        Args:
+            img (cv::mat): src image
 
-
-
-
-
-
+        Returns:
+            predict (int): predict result of the SVM
+        """
+        # 计算输入图像的HOG特征
+        winSize = (48, 48)
+        blockSize = (16, 16)
+        blockStride = (8, 8)
+        cellSize = (8, 8)
+        nbins = 9
+        img = cv2.resize(img, (48, 48))
+        hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins, 1, -1,
+                                cv2.HOGDescriptor_L2Hys, 0.2, False, cv2.HOGDescriptor_DEFAULT_NLEVELS)
+        hog_data = hog.compute(img)
+        # int转换为float类型， 防止报错，并作转置，输入SVM的数据大小为(1,900)
+        hog_data = hog_data.astype(np.float32)
+        hog_data = np.transpose(hog_data)
+        # 创建SVM 当前执行一次识别需要重新读取一次，考虑将其放入init函数中
+        svm = cv2.ml.SVM_load(self.svm_model_path)
+        # predict为SVM识别结果 1为待击打 2为已击打
+        _, predict = cv2.ml_StatModel.predict(svm, hog_data)
+        return predict
 
